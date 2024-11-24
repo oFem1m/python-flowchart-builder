5e862179f594cabe479f6183c06b248599f3727e
alex <volohovaleksandr1974@gmail.com>
2024-11-24 15:53:04 +0300

    обновленный тест

--- backend/test_file.py
+++ backend/test_file.py
@@ -2,6 +2,9 @@ def sample_function(x):
     if x < 0:
         x += 5
     elif x == 0:
+        s = 0
+        while s < 3:
+            s += 1
         return 0
     else:
         for i in range(5):
