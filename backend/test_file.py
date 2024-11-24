9b37add90d10299e596fe735f04711fd906064a0
alex <volohovaleksandr1974@gmail.com>
2024-11-24 15:14:35 +0300

    вернулся к хорошему парсеру

--- backend/test_file.py
+++ backend/test_file.py
@@ -1,8 +1,11 @@
 def sample_function(x):
-    if x > 0:
-        for i in range(x):
-            print(i)
+    if x < 0:
+        x += 5
+    elif x == 0:
+        return 0
     else:
-        while x < 5:
-            x += 1
+        for i in range(5):
+            print(i)
+        x -= 5
+    y = 6
     return x
